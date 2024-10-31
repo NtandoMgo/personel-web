bcefe7ac9797cf94ee9d61c76cf4f792f32ac93b
Ntando <mngnta011@myuct.ac.za>
2024-10-31 22:20:01 +0200

    Project categiries update

--- src/utlits/fackData/projectData.jsx
+++ src/utlits/fackData/projectData.jsx
@@ -7,38 +7,49 @@ export const projectsData = [
     {
         id: 1,
         src: work_1,
-        category: "Design",
-        title: "Mobile Application Design",
+        category: "Web Development",
+        title: "URL Shortener Service",
     },
     {
         id: 2,
         src: work_2,
-        category: "Game",
-        title: "Website Makeup Design",
+        category: "Web Development",
+        title: "User Authentication System",
     },
     {
         id: 3,
         src: work_3,
-        category: "Branding",
-        title: "Brand Identity and Motion Design",
+        category: "Web Development",
+        title: "Employee Management System",
     },
     {
         id: 4,
         src: work_4,
-        category: "Marketing",
-        title: "Mobile Application Design",
+        category: "Mobile Application Design",
+        title: "Student Grade Calculator",
     },
     {
         id: 5,
-        src: work_3,
-        category: "Design",
-        title: "Brand Identity and Motion Design",
+        src: work_1,
+        category: "Mobile Application Design",
+        title: "Mobile Design",
     },
     {
         id: 6,
-        src: work_1,
-        category: "Marketing",
-        title: "Brand Identity and Motion Design"
+        src: work_2,
+        category: "Software Development",
+        title: "C#.Net-based Hotel Booking System",
     },
-
-]
+    {
+        id: 7,
+        src: work_3,
+        category: "Software Development",
+        title: "MedleyRaceSimulation",
+    },
+    {
+        id: 8,
+        src: work_4,
+        category: "Branding and Design",
+        title: "Customizable Single-Page Website Template",
+    },
+];
